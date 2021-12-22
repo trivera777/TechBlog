2b9c99050b9733ac527f7f47bb2627724696b934
Tifanny Rivera <trivera51580@gmail.com>
2021-12-21 22:35:19 -0700

    added screenshot to readme

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,7 +1,7 @@
-const router = require('express').Router();
-const { Post, User, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
+const router = require('express').Router();
 const withAuth = require('../../utils/auth');
+const { Post, User, Comment } = require('../../models');
 
 router.get('/', (req, res) => {
     Post.findAll({
